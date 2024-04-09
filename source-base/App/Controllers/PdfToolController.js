73f902242b3cefeede7c6e17a63f6f3dda1ce8d1
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-04-09 13:34:49 +0700

    Optimal agothism : delete Page

--- source-base/App/Controllers/PdfToolController.js
+++ source-base/App/Controllers/PdfToolController.js
@@ -10,51 +10,54 @@ class PdfToolController {
     pdfSplit(req,res) {
         res.render('pdf-split');
     }
-    pdfUpload(req, res) {
-        const upload = multer({
-            dest: 'source-base/Uploads/',
-            fileFilter: function (req, file, cb) {
-                if (file.originalname.endsWith('.pdf')) {
-                    cb(null, true);
-                } else {
-                    cb(new Error('Only PDF files are allowed!'));
+    async pdfUpload(req, res) {
+        try {
+            const upload = multer({
+                dest: 'source-base/Uploads/',
+                fileFilter: function (req, file, cb) {
+                    if (file.originalname.endsWith('.pdf')) {
+                        cb(null, true);
+                    } else {
+                        cb(new Error('Only PDF files are allowed!'));
+                    }
                 }
-            }
-        }).single('pdfFilePath');
-        upload(req, res, async function (err) {
-            if (err) {
-                console.error('Error uploading file:', err);
-                return res.status(500).send('Internal Server Error');
-            }   
-            const pdfFilePath = req.file.path;
-            pdfTemp = pdfFilePath;
-        res.redirect(`/pdf-tools/pdf-split/option?pdfFilePath=${pdfFilePath}`);
-        });
+            }).single('pdfFilePath');
+
+            upload(req, res, async function (err) {
+                if (err) {
+                    console.error('Error uploading file:', err);
+                    return res.status(500).send('Internal Server Error');
+                }   
+                const pdfFilePath = req.file.path;
+                pdfTemp = pdfFilePath;
+                res.redirect(`/pdf-tools/pdf-split/option?pdfFilePath=${pdfFilePath}`);
+            });
+        } catch (error) {
+            console.error('Error uploading file:', error);
+            return res.status(500).send('Internal Server Error');
+        }
     }
-    pdfOption_Index(req,res){
+    pdfOption_Index(req, res){
         const pdfFilePath = req.query.pdfFilePath;
         res.render('pdf-split_option', {pdfFilePath});
     }
-    pdfSplit_Resolve = async (req, res) => {
-        const indexPage = req.body.indexPage;
-        const pdfFilePath = pdfTemp;
+    async pdfSplit_Resolve (req, res) {
         try {
+        const indexPageBasic = req.body.indexPageBasic;
+        const indexPageSmart = req.body.indexPageSmart;
+        const pdfFilePath = pdfTemp;
+        const pagesToDelete = parseInput(indexPageBasic, indexPageSmart);
+        console.log(pagesToDelete)
             const pdfData = await fs.readFile(pdfFilePath);
             const pdfDataArray = new Uint8Array(pdfData);
             const pdfDoc = await PDFDocument.load(pdfDataArray);
             const totalPages = pdfDoc.getPageCount();
-            const pagesToDelete = indexPage.split(',').map(page => parseInt(page.trim()) - 1);
-    
             if (pagesToDelete.some(pageNumber => pageNumber < 0 || pageNumber >= totalPages)) {
-                throw new Error('Chỉ mục trang không hợp lệ');
+                throw new Error(`Invalid page index. Please provide page indices within our format or range 0 to ${totalPages}`);
             }
-    
-            pagesToDelete.sort((a, b) => a - b);
-    
             for (let i = pagesToDelete.length - 1; i >= 0; i--) {
                 pdfDoc.removePage(pagesToDelete[i]);
             }
-    
             const modifiedPdfBytes = await pdfDoc.save();
             const pdfFileName = path.basename(pdfFilePath);
             const pdfFilePathWithExtension = pdfFileName.endsWith('.pdf') ? pdfFilePath : `${pdfFilePath}.pdf`;
@@ -78,6 +81,19 @@ class PdfToolController {
             }
         });
     }
+}
+function parseInput(inputBasic,inputSmart) {
+    const pagesToDelete = inputBasic.split(',').map(value => parseInt(value.trim()) - 1);
+    const [start, end] = inputSmart.split('=>').map(value => parseInt(value.trim()) - 1);
+    const result = new Set();
+    for (let i = pagesToDelete.length - 1; i >= 1; i--) {
+        result.add(pagesToDelete[i]); 
+    }
+    for (let i = start; i <= end; i++) {
+        result.add(i);
+    }
+    
+    return Array.from(result).sort((a, b) => a - b);
 }
 
 module.exports = new PdfToolController();
