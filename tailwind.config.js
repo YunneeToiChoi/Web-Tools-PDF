4d26f2c008cb67d9fa894a7a20bb716442fa3bab
Nguyễn Hoàng Phong <138206785+Pwindy2611@users.noreply.github.com>
2024-04-14 21:29:52 +0700

    signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,34 +2,41 @@
 module.exports = {
 
   content: ["./source-base/**/*.{hbs,html,js}"],//cho phép tailwind trỏ đến những tệp html và js trong source base để phát hiện những class đã dùng đã loại bỏ purge vì tailwind đã cải tiến không impport những class không dùng
-  // darkMode: ['variant', [
-  //   '@media (prefers-color-scheme: dark) { &:not(.light *) }',
-  //   '&:is(.dark *)',
-  // ]],
   theme: {
-    // screens: {
-    //   sm: '480px',
-    //   md: '768px',
-    //   lg: '976px',
-    //   xl: '1440px',
-    // },
     colors: {
       'hover-btn-nav':'#242424',
       'primary-home':'#7961f2',
     },
-    // fontFamily: {
-    //   sans: ['Graphik', 'sans-serif'],
-    //   serif: ['Merriweather', 'serif'],
-    // },
     extend: {
-      // spacing: {
-      //   '128': '32rem',
-      //   '144': '36rem',
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+        'fadeIn':'fadeIn 0.2s ease-in',
+        'fadeOut':'fadeOut 0.2s ease-in',
+        'fadeInRight':'fadeInRight 0.2s ease-in',
+        'fadeOutRight':'fadeOutRight 0.2s ease-in'
       },
-    //   borderRadius: {
-    //     '4xl': '2rem',
-    //   }
-    // }
+      keyframes:
+      {
+        fadeIn:{
+          'from': { opacity: 0},
+          'to': {  opacity: 1},
+        },
+
+        fadeOut:{
+          'from': { opacity: 1},
+          'to': {  opacity: 2},
+        },
+
+        fadeInRight: { 
+          'from': { opacity:0, transform: 'translate3d(100%, 0, 0)'},
+          'to': { opacity:1, transform: 'translate3d(0, 0, 0)'},
+        },
+        fadeOutRight: { 
+          'from':{opacity:1},
+          'to': {opacity:0, transform: 'translate3d(100%, 0, 0)'},
+        },
+      }
+    },
   },
   plugins: [],
 }
