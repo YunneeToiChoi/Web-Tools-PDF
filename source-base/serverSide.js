45b6cb4891461d510a88fd6205a3568f8bdf39a5
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-04-17 17:27:33 +0700

    Update serverSide.js

--- source-base/serverSide.js
+++ source-base/serverSide.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const connectDB = require('./config/connectDB');
 // LIBRARIES SUPPORTED
 const morgan = require('morgan'); // log 
