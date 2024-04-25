d11536f231739cb814e65a5755f598b465787e78
Phan Văn Năng <109004626+YunneeToiChoi@users.noreply.github.com>
2024-04-25 16:43:24 +0700

    compare hahPassword

--- source-base/App/Controllers/UserController.js
+++ source-base/App/Controllers/UserController.js
@@ -1,12 +1,22 @@
+const bcrypt = require('bcrypt');
 const db = require('../../models/index');
-class UserController{
-     handleLogin = (req,res) => {
+class UserController {
+    handleLogin = async (req, res) => {
         let email = req.body.email;
         let password = req.body.password;
-        return res.status(200).json({
-            yourEmail : email,
-            yourPassword : password
-        })
+        const user = await db.User.findOne({ email: email });
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+
+        if (passwordCompare) {
+            return res.status(200).json({ message: 'ok' });
+        } else {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
     }
 }
 module.exports = new UserController();
